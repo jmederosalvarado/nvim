dd275c4536540bc18f4b8aaee9eab66da4f6ec7a
Jorge Mederos Alvarado <jmederosalvarado@gmail.com>
2021-10-19 20:57:52 -0400

    Remove arrows from lualine separators

--- lua/config/lualine.lua
+++ lua/config/lualine.lua
@@ -2,8 +2,8 @@ require("lualine").setup({
 	options = {
 		icons_enabled = true,
 		theme = "tokyonight",
-		-- component_separators = { "│", "│" },
-		-- section_separators = { "", "" },
+		component_separators = { "│", "│" },
+		section_separators = { "", "" },
 		disabled_filetypes = {},
 	},
 	sections = {
