2e03f6555b2305b89e6ef0343ffd96d9717e2c8c
Jorge Mederos <jmederosalvarado@gmail.com>
2023-10-17 15:45:07 +0200

    remove omnisharper

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -144,45 +144,6 @@ return {
 					})
 				end,
 
-				["omnisharp"] = function()
-					-- require("omnisharper").setup({
-					-- 	cmd = require("mason-lspconfig.server_configurations.omnisharp")().cmd,
-					-- 	capabilities = capabilities,
-					-- 	on_attach = on_attach,
-					-- })
-
-					-- require("lspconfig").omnisharp.setup({
-					-- 	capabilities = capabilities,
-					-- 	on_attach = on_attach,
-					-- 	handlers = {
-					-- 		["textDocument/definition"] = require("omnisharp_extended").handler,
-					-- 	},
-					--
-					-- 	enable_import_completion = false,
-					-- })
-					--
-					-- vim.api.nvim_create_autocmd("LspAttach", {
-					-- 	callback = function(ev)
-					-- 		local client = vim.lsp.get_client_by_id(ev.data.client_id)
-					-- 		local function toSnakeCase(str)
-					-- 			return string.gsub(str, "%s*[- ]%s*", "_")
-					-- 		end
-					--
-					-- 		if client.name == "omnisharp" then
-					-- 			local tokenModifiers =
-					-- 				client.server_capabilities.semanticTokensProvider.legend.tokenModifiers
-					-- 			for i, v in ipairs(tokenModifiers) do
-					-- 				tokenModifiers[i] = toSnakeCase(v)
-					-- 			end
-					-- 			local tokenTypes = client.server_capabilities.semanticTokensProvider.legend.tokenTypes
-					-- 			for i, v in ipairs(tokenTypes) do
-					-- 				tokenTypes[i] = toSnakeCase(v)
-					-- 			end
-					-- 		end
-					-- 	end,
-					-- })
-				end,
-
 				["zls"] = function()
 					require("lspconfig").zls.setup({
 						on_attach = on_attach,
