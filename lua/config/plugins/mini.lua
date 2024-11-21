18af2ccddcc16bbe78f363c2e8377c112c0ca803
Jorge Mederos <jmederosalvarado@gmail.com>
2024-11-21 08:37:49 +0100

    commit

--- lua/config/plugins/mini.lua
+++ lua/config/plugins/mini.lua
@@ -1,50 +1,59 @@
--- return and configure
-local specs = {
-	{
-		"echasnovski/mini.nvim",
-		config = function()
-			-- Better Around/Inside textobjects
-			--
-			-- Examples:
-			--  - va)  - [V]isually select [A]round [)]paren
-			--  - yinq - [Y]ank [I]nside [N]ext [']quote
-			--  - ci'  - [C]hange [I]nside [']quote
-			require("mini.ai").setup({ n_lines = 500 })
+local mini = {}
 
-			-- Add/delete/replace surroundings (brackets, quotes, etc.)
-			--
-			-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-			-- - sd'   - [S]urround [D]elete [']quotes
-			-- - sr)'  - [S]urround [R]eplace [)] [']
-			require("mini.surround").setup()
+mini["ai"] = {
+	enabled = true,
+}
 
-			require("mini.pairs").setup()
+mini["surround"] = {
+	enabled = true,
+}
 
-			local statusline = require("mini.statusline")
-			statusline.setup({ use_icons = vim.g.have_nerd_font })
-			---@diagnostic disable-next-line: duplicate-set-field
-			statusline.section_location = function()
-				return "%2l:%-2v"
-			end
-		end,
-	},
+mini["bracketed"] = {
+	enabled = true,
+}
 
-	{
-		"echasnovski/mini.icons",
-		opts = {},
-		lazy = true,
-		specs = {
-			{ "nvim-tree/nvim-web-devicons", enabled = false, optional = true },
-		},
-		init = function()
-			package.preload["nvim-web-devicons"] = function()
-				-- needed since it will be false when loading and mini will fail
-				package.loaded["nvim-web-devicons"] = {}
-				require("mini.icons").mock_nvim_web_devicons()
-				return package.loaded["nvim-web-devicons"]
-			end
-		end,
+mini["comment"] = {
+	enabled = true,
+}
+
+mini["diff"] = {
+	enabled = true,
+}
+
+mini["hues"] = {
+	enabled = true,
+	opts = {
+		background = "#351721",
+		foreground = "#cdc4c6",
 	},
 }
 
-return {}
+mini["icons"] = {
+	enabled = true,
+	opts = {
+		background = "#351721",
+		foreground = "#cdc4c6",
+	},
+}
+
+return {
+	"echasnovski/mini.nvim",
+	enabled = true,
+	config = function()
+		for mod, plugin in pairs(mini) do
+			if not plugin.enabled then
+				goto continue
+			end
+
+			if type(plugin.config) == "function" then
+				plugin.config()
+			elseif type(plugin.opts) == "table" then
+				require("mini." .. mod).setup(plugin.opts)
+			else
+				require("mini." .. mod).setup({})
+			end
+
+			::continue::
+		end
+	end,
+}
