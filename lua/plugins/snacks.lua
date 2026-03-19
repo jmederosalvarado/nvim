9faa89b9598e9caeab01eae2763a9a196c83b16d
Jorge Mederos <46798594+jmederosalvarado@users.noreply.github.com>
2026-03-19 13:24:18 +0100

    refactor(theme): remove catppuccin theme config

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -6,16 +6,12 @@ return {
   ---@module 'snacks'
   ---@type snacks.Config
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
     bigfile = { enabled = true },
     indent = { enabled = true },
     input = { enabled = true },
     notifier = { enabled = true },
     quickfile = { enabled = true },
     scope = { enabled = true },
-    -- scroll = { enabled = true },
     statuscolumn = { enabled = true },
     words = { enabled = true },
   },
