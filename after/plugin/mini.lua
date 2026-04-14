8d075bd7a3a5b0d32bf3e095d0063bac04c7d43a
Jorge Mederos <46798594+jmederosalvarado@users.noreply.github.com>
2026-04-14 14:37:59 +0200

    refactor(mini): disable session autoread

--- after/plugin/mini.lua
+++ after/plugin/mini.lua
@@ -1,20 +1,10 @@
 require("mini.ai").setup()
 require("mini.align").setup()
 require("mini.sessions").setup({
-  autoread = true,
+  autoread = false,
   autowrite = true,
 })
 require("mini.surround").setup()
 
 require("mini.bracketed").setup()
 require("mini.notify").setup()
-
--- require("mini.diff").setup({
---   view = {
---     style = "sign",
---   },
--- })
-
--- vim.keymap.set("n", "<leader>ph", function()
---   require("mini.diff").toggle_overlay(0)
--- end, { desc = "Toggle diff overlay in current buffer" })
